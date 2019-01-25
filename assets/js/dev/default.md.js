031affdf555384817a5f53847f9c071d53bb4531
Lcfvs <michael.rouges@gmail.com>
2019-01-25 08:35:00 +0100

    + anticore/middleware/main/mono

--- assets/js/dev/default.md.js
+++ assets/js/dev/default.md.js
@@ -1,8 +1,7 @@
 import {anticore} from 'anticore'
-import {one} from 'anticore/dom/query/one'
-import {replace} from 'anticore/dom/tree/replace'
 
-anticore.on('.default', function(element, next, loaded) {
-  loaded && replace(element, one('main'))
+anticore.on('.default', function (element, next, loaded) {
+  // Put your code here
+
   next()
 })
--- assets/js/dev/index.js
+++ assets/js/dev/index.js
@@ -1,4 +1,8 @@
 import {anticore} from 'anticore'
+
+// Import your own middlewares here
 import './default.md.js'
 
+// Let the following lines at the end of this file
+import 'anticore/middleware/main/mono'
 anticore.defaults().populate()
