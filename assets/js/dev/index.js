e086d3e074bafa90c4eba22f38f97b7c32ad9756
Lcfvs <michael.rouges@gmail.com>
2019-01-27 03:00:10 +0100

    improving demo

--- assets/js/dev/index.js
+++ assets/js/dev/index.js
@@ -1,6 +1,5 @@
 import {anticore} from 'anticore'
 // Import your own middlewares here
-import './nav'
 
 // Let the following lines at the end of this file
 import 'anticore/middleware/main/mono'
--- assets/js/dev/nav.js
+++
@@ -1,8 +0,0 @@
-import {anticore} from 'anticore'
-import {one} from 'anticore/dom/query/one'
-
-anticore.on('main', function (element, next, loaded, url) {
-  one('body nav a.current').classList.remove('current')
-  one('body nav a[href="' + url + '"]').classList.add('current')
-  next()
-})
